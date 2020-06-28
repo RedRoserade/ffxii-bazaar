3378b123bee8b92f2f572fc2b8c03ac1fabe5c8c
André Carvalho <afecarvalho@gmail.com>
2020-06-28 16:34:07 +0100

    Only use Suspense on the part that needs it

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { UpdateToastDisplay } from "./UpdateToastDisplay";
 
 import { PageLoading } from "./PageLoading";
 import { SelectedRecipesHolder, SelectedRecipesLabel } from "./SelectedRecipes";
-import { ItemListForSelectedRecipes } from "./ItemListForSelectedRecipes";
 
 const RecipeDetails = React.lazy(() => import("./RecipeDetails").then((x) => ({ default: x.RecipeDetails })));
 
@@ -18,12 +17,16 @@ const ItemDetails = React.lazy(() => import("./ItemDetails").then((x) => ({ defa
 
 const Items = React.lazy(() => import("./Items").then((x) => ({ default: x.Items })));
 
+const ItemListForSelectedRecipes = React.lazy(() =>
+  import("./ItemListForSelectedRecipes").then((x) => ({ default: x.ItemListForSelectedRecipes }))
+);
+
 class App extends React.Component {
   public render() {
     return (
       <HashRouter>
-        <React.Suspense fallback={<PageLoading />}>
-          <SelectedRecipesHolder>
+        <SelectedRecipesHolder>
+          <React.Suspense fallback={<PageLoading />}>
             <main className="MainContents">
               <Switch>
                 <Route path="/recipes/:id" component={RecipeDetails} />
@@ -39,23 +42,23 @@ class App extends React.Component {
                 <Redirect from="/" to="/recipes" />
               </Switch>
             </main>
-            <nav className="MainNav">
-              <NavLink to="/recipes" replace={true}>
-                Recipes
-              </NavLink>
-              <NavLink to="/items" replace={true}>
-                Items
-              </NavLink>
-              <NavLink to="/about" replace={true}>
-                About
-              </NavLink>
-              <NavLink to="/sell-list" replace={false}>
-                <SelectedRecipesLabel />
-              </NavLink>
-            </nav>
-            <UpdateToastDisplay />
-          </SelectedRecipesHolder>
-        </React.Suspense>
+          </React.Suspense>
+          <nav className="MainNav">
+            <NavLink to="/recipes" replace={true}>
+              Recipes
+            </NavLink>
+            <NavLink to="/items" replace={true}>
+              Items
+            </NavLink>
+            <NavLink to="/about" replace={true}>
+              About
+            </NavLink>
+            <NavLink to="/sell-list" replace={false}>
+              <SelectedRecipesLabel />
+            </NavLink>
+          </nav>
+          <UpdateToastDisplay />
+        </SelectedRecipesHolder>
       </HashRouter>
     );
   }
